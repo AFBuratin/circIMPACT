ef4da20b05bba21f1df8d9a1331bfc7dcab213ee
ABuratin <alessia.buratin.phd@gmail.com>
2020-10-28 14:52:28 +0100

    finished the first part of pipeline until definition of gene deregulated using circRNA expression as stratificator of samples

---
+++ R/circTarget_functions.R
@@ -0,0 +1,192 @@
+#' Define colors of condition
+#'
+#' @param n number of condion
+#'
+#' @return data.frame of color hues per condition
+#' 
+#'
+#' @export
+gg_color_hue <- function(n) {
+  hues = seq(20, 360, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+  # hcl_palettes("sequential (multi-hue)", n = n, palette = "Plasma")
+}
+
+#' Define colors of condition
+#'
+#' @param meta meta file
+#'
+#' @return data.frame of color hex and hue per condition
+#' 
+#'
+#' @export
+get.color.hues <- function(meta){
+  intgroup.dt <- meta[, .(.N), by = .(sample_id, 
+                                      condition)][order(sample_id), 
+                                                  .(sample_id), by = condition]
+  samples.per.condition <- intgroup.dt[, .N, by = .(condition)]
+  rownames(samples.per.condition) <- samples.per.condition$condition
+  n.conditions <- nrow(samples.per.condition)
+  
+  if(n.conditions <= 12){ 
+    palette <- "Set3"
+    if(n.conditions <= 8) palette <- "Set2"
+    hues <- brewer_pal(palette = palette)(n.conditions)
+    if(palette == "Set3"){
+      hues[2] <- brewer_pal(palette = "Set1")(1)
+      if(length(hues) == 12){
+        hues[12] <- brewer_pal(palette = "Set1")(9)[7]
+      }
+    }
+  }else{
+    hues <- gg_color_hue(n.conditions)
+  }
+  
+  if(nrow(intgroup.dt) == 1){
+    intgroup.dt[, `:=`(color = hues[1],  hue = hues[1])]
+  }else{
+    for(i in 1:n.conditions){
+      n.hues <- samples.per.condition[i, "N"] + 2
+      col.hues <- colorRampPalette(colors = c(hues[i], 
+                                              "white"))(n.hues$N)[1:(n.hues$N-2)]
+      
+      intgroup.dt[condition == rownames(samples.per.condition)[i], `:=`(color = col.hues,
+                                                                        hue = hues[i])]
+    }
+  }
+  intgroup.dt[]
+}
+
+
+
+#' Detect circRNA markers
+#' To establish a possible impact of circRNA in gene expression
+#' we define two groups of samples defined by similarity in circRNAs expression.
+#' K-means algorithm is used to define these two groups and then DESeq test (adj. p-value<.01) 
+#' have been used to detect circRNA markers.
+#'
+#' @param circ.m circRNA id
+#' @param dds DeseqDataSet object
+#' @param sf sizefactor  
+#' @param k number of group in K-means algorithm 
+#' @param method.d method for calculate matrix distance
+#' @param method.c method for clustering
+#' 
+#' @return data.frame of padj
+#' 
+#'
+#' @export
+marker.detection <- function(circ.m, dds, sf, method.d, method.c, k){
+  
+  # if(median(circ.m)==0){
+  #   group <- ifelse(circ.m<mean(circ.m), "DW", "UP")
+  # } else {
+  #   group <- ifelse(circ.m<median(circ.m), "DW", "UP")
+  # }
+  
+  dist_mat <- dist(as.data.frame(scale(circ.m)), method = method.d)
+  hclust_avg <- hclust(dist_mat, method = method.c)
+  cut_avg <- cutree(hclust_avg, k = k)
+  group <- ifelse(cut_avg==1, "g1", "g2")
+  colData(dds)$group <- as.factor(group)
+  design(dds) <- ~group
+  sizeFactors(dds) <- sf
+  dds <- estimateDispersionsGeneEst(dds)
+  dispersions(dds) <- mcols(dds)$dispGeneEst
+  dds <- nbinomWaldTest(dds)
+  padj =  as.data.frame(results(dds))
+  group = t(as.data.frame(colData(dds)$group))
+  
+  return(res = cbind(padj, group))
+}
+
+#' Select circRNA markers
+#' To establish a possible impact of circRNA in gene expression
+#' we select a subset of circRNA defined as markers: 
+#' only circRNA which expression can discretize two groups 
+#' (DESeq adj. p-value<.01) have been selected.
+#'
+#' @param dat circRNA data set
+#' @param dds DeseqDataSet object filterd
+#' @param sf sizefactor  
+#' @param p.cutoff select only circRNAs signficantly differentially expressed
+#' @param lfc.cutoff log fold change cutoff to select circRNAs highly deregulated
+#' @param k number of group in K-means algorithm 
+#' @param method.d method for calculate matrix distance
+#' @param method.c method for clustering
+#' 
+#' 
+#' @return list of two elements: circRNAs estimate, circRNA-target ids
+#' 
+#'
+#' @export
+marker.selection <- function(dat, dds, sf, p.cutoff, lfc.cutoff, method.d, method.c, k){
+  library(doParallel)
+  library(dplyr)
+  library(tidyr)
+  no_cores <- detectCores() - 1  
+  registerDoParallel(cores=no_cores)  
+  
+  circ_mark = foreach::foreach(i=1:nrow(dat), .combine=rbind) %dopar% {
+  
+  #make a for loop to estimate log2FC and p.adj to select marker circRNAs
+  results.temp <- marker.detection(circ.m = dat[i,], dds = dds[i,], 
+                                               sf = sf, method.d = method.d, 
+                                               method.c = method.c, k = k)
+
+  }
+
+  circ_mark_selection <- circ_mark %>% dplyr::mutate(circ_id = rownames(data.filt)) %>% 
+    tidyr::drop_na() %>% 
+    dplyr::filter(padj<=p.cutoff) %>% dplyr::filter(log2FoldChange>lfc.cutoff) %>% 
+    arrange(abs(log2FoldChange))
+
+  markers.circrnas = circ_mark_selection$circ_id
+  group = data.frame(circ_mark[,-c(1:6)], circ_id = rownames(circ_mark))
+  group.df = gather(group, key="sample_id", value = "group", -circ_id) %>% arrange(circ_id)
+  return(list(circ.mark = circ_mark, circ.targetIDS = markers.circrnas, group.df = group.df))
+}
+  
+#' Detect dergulated genes using circRNA-markers as stratificator of samples
+#'
+#' @param circ_idofinterest circRNA-marker ID
+#' @param circRNAs normalized read count of circRNAs
+#' @param linearRNAs row read count of gene
+#' @param meta meta file
+#' @param colData coldata dataframe
+#' @param covariates additinal covariates for batch effect
+#' 
+#' @return data.frame of gene deregulated per circRNA-markers
+#' 
+#'
+#' @export
+geneexpression <- function(circ_idofinterest, circRNAs, linearRNAs, group, colData, covariates, padj){
+  
+  ## define covariates of interest: circlow vs high
+  # circ_sample <- circRNAs %>% as.data.table() %>% dplyr::filter(circ_id==circ_idofinterest) %>% dplyr::select_if(is.numeric) %>% 
+  #   gather(sample_id, sample_val) %>% dplyr::filter(sample_val>=median(sample_val))
+  # circ_sample <- merge(circ_sample, colData, by.x = "sample_id", by.y = "sample_id3")
+  # geneofinterest <- circ_to_genes$gene_names[circ_to_genes$circ_id==circ_idofinterest]
+  # meta <- colData %>% mutate(circKD = if_else(sample_id3%in%circ_sample$sample_id, paste0(geneofinterest, "_high"), paste0(geneofinterest, "_low")))
+  
+  colData = colData %>% 
+    mutate(circM = if_esle(sample_id%in%group$sample_id[group$group=="g1"]), paste0(circ_idofinterest, "g1"), paste0(circ_idofinterest, "g2"))
+  
+  
+  # colData$circKD <- meta$circKD[match(colData$sample_id, meta$sample_id)]
+  colData$circM <- factor(colData$circM)
+  colData$condition <- factor(colData$condition)
+  
+  ## make deseqdataset for test
+  dds <- DESeqDataSetFromMatrix(countData = ceiling(filt.mat[, rownames(colData)]),
+                                colData = colData,
+                                design = as.formula(paste0("~", covariates, "+ circM")))
+  
+  ## performe DEGs
+  dds <- DESeq(dds, fitType = "local")
+  res <- results(dds)
+  n.degs <- sum(res$padj <= padj, na.rm = T)
+  degs <- res[which(res$padj <= padj), ]
+  
+  return(as.data.frame(cbind(res.dt, n.degs)))
+}
--- vignettes/CircTarget.R
+++ vignettes/CircTarget.R
@@ -4,10 +4,82 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ---- fig.show='hold'---------------------------------------------------------
-plot(1:10)
-plot(10:1)
+## ----eval = FALSE-------------------------------------------------------------
+#  devtools::install_github("ABuratin/CircTarget")
 
-## ---- echo=FALSE, results='asis'----------------------------------------------
-knitr::kable(head(mtcars, 10))
+## ----setup, warning = FALSE, message = FALSE----------------------------------
+library(CircTarget)
+library(DESeq2)
+library(data.table)
+library(plyr)
+library(Rtsne)
+
+## -----------------------------------------------------------------------------
+data("circularData")
+data("count.matrix")
+data("meta")
+data("coldata.df")
+## define sample/condition colors
+intgroup.dt <- meta[, .(.N), by = .(sample_id, 
+                                      condition)][order(sample_id), 
+                                                  .(sample_id), by = condition]
+samples.per.condition <- data.frame(intgroup.dt[, .N, by = .(condition)], row.names = "condition")
+  
+n.conditions <- nrow(samples.per.condition)
+hues <- CircTarget::gg_color_hue(n.conditions)
+for(i in 1:n.conditions){
+      n.hues <- samples.per.condition[i, "N"] + 2
+      col.hues <- colorRampPalette(colors = c(hues[i], 
+                                              "white"))(n.hues)[1:(n.hues-2)]
+      
+      intgroup.dt[condition == rownames(samples.per.condition)[i], `:=`(color = col.hues,
+                                                                        hue = hues[i])]
+    }
+## create a deseq dataset object 
+dds.circular <- suppressMessages(DESeqDataSetFromMatrix(countData = ceiling(circularData[, coldata.df$sample_id[order(coldata.df$condition)]]),
+                                   colData = coldata.df[order(coldata.df$condition),],
+                                   design = ~ condition))
+dds.circular <- suppressMessages(estimateSizeFactors(dds.circular))
+sf <- sizeFactors(dds.circular)
+
+## -----------------------------------------------------------------------------
+data.filt <- circularData[rowSums(circularData >= 5) >= 3,]
+dds.filt.expr <- suppressMessages(DESeqDataSetFromMatrix(countData = ceiling(data.filt[,coldata.df$sample_id[order(coldata.df$condition)]]),
+                                   colData = coldata.df[order(coldata.df$condition),],
+                                   design = ~ condition))
+dds.filt.expr <- suppressMessages(estimateSizeFactors(dds.filt.expr))
+sf.filt <- sizeFactors(dds.filt.expr)
+
+
+## ----message=FALSE, warning=FALSE---------------------------------------------
+
+circtarget <- marker.selection(dat = data.filt, dds = dds.filt.expr, sf = sf, p.cutoff = 0.1, lfc.cutoff = 1.5, 
+                               method.d = "euclidean", method.c = "average", k = 2)
+
+
+## -----------------------------------------------------------------------------
+markers.circrnas <- circtarget$circ.targetIDS
+mat.filt.mark <- circularData[markers.circrnas, ]
+
+dds.filt.mark <- DESeqDataSetFromMatrix(countData = ceiling(mat.filt.mark[,coldata.df$sample_id[order(coldata.df$condition)]]),
+                                   colData = coldata.df[order(coldata.df$condition),],
+                                   design = ~ 1)
+dds.filt.vst <- varianceStabilizingTransformation(dds.filt.mark, fitType = "local", blind = F)
+norm.counts.filt <- assay(dds.filt.vst)
+
+## -----------------------------------------------------------------------------
+dt <- norm.counts.filt
+
+
+## -----------------------------------------------------------------------------
+## Rtsne function may take some minutes to complete...
+set.seed(9)
+mydist <- dist(t(norm.counts.filt))
+## t-SNE representation
+tsne_data <- Rtsne(mydist, pca = F, perplexity=5, max_iter=5000)
+
+## getting the two dimension matrix
+d_tsne_1 = as.data.frame(tsne_data$Y)
+rownames(d_tsne_1) <- colnames(norm.counts.filt)
+
 
